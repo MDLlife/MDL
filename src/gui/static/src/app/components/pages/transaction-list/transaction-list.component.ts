2d10831839fb496a3a84b74325b8079cf41d60c5
dan-leech <dan-leech@mail.ru>
2018-03-02 16:33:57 +0300

    TransactionList component

--- src/gui/static/src/app/components/pages/transaction-list/transaction-list.component.ts
+++ src/gui/static/src/app/components/pages/transaction-list/transaction-list.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { WalletService } from '../../../services/wallet.service';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-transaction-list',
   templateUrl: './transaction-list.component.html',
-  styleUrls: ['./transaction-list.component.sass']
+  styleUrls: ['./transaction-list.component.scss']
 })
-export class TransactionListComponent implements OnInit {
+export class TransactionListComponent implements OnInit, OnDestroy {
+  transactions: any[];
 
-  constructor() { }
+  private priceSubscription: Subscription;
+
+  constructor(
+    private walletService: WalletService,
+  ) { }
 
   ngOnInit() {
+    this.walletService.transactions().subscribe(transactions => this.transactions = transactions);
   }
 
+  ngOnDestroy() {
+    this.priceSubscription.unsubscribe();
+  }
 }
