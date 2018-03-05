394193b28b4550b3b92dbe681103023b53f85b55
Roman <roman@tronenko.net>
2018-03-05 15:06:38 +0800

    New wallet GUI, SKY -> MDL, but some places might still have sky mentioned

--- src/util/droplet/droplet_test.go
+++ src/util/droplet/droplet_test.go
@@ -72,8 +72,8 @@ func TestFromString(t *testing.T) {
 			n: 123000456,
 		},
 		{
-			s: "100SKY",
-			e: errors.New("can't convert 100SKY to decimal"),
+			s: "MDL",
+			e: errors.New("can't convert 100MDL to decimal"),
 		},
 		{
 			s: "",
